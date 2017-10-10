c54038e28a59881a568c47b8a1dc3d7e00f0284e
Yunsoo Jung <yunsoo3042@gmail.com>
2017-10-10 16:41:12 +0900

    chage sourcedoc serializer

--- TaxPaasServer/django-app/autoinput/serializers/__init__.py
+++ TaxPaasServer/django-app/autoinput/serializers/__init__.py
@@ -1,2 +1,3 @@
 from .w2 import *
 from .taskserializer import *
+from .sourcedoc import *
--- TaxPaasServer/django-app/autoinput/serializers/sourcedoc.py
+++ TaxPaasServer/django-app/autoinput/serializers/sourcedoc.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from autoinput.models import sourcedoc
 
+__all__ = ("SourceDocSerializer", )
+
 
 class SourceDocSerializer(serializers.ModelSerializer):
 
