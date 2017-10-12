68099ee1bb8f72af44793d465c26cbabc86c6ba1
Yunsoo Jung <yunsoo3042@gmail.com>
2017-10-12 10:30:36 +0900

    change CATEGORY_CHOICE's 'C': 'Child' to 'D': 'Dependents'

--- TaxPaasServer/django-app/autoinput/models/sourcedoc.py
+++ TaxPaasServer/django-app/autoinput/models/sourcedoc.py
@@ -1,16 +1,15 @@
 from django.db import models
-from member.models import TaxPayerProfile
+
 __all__ = ('SourceDoc', )
 
 CATEGORY_CHOICE = (('M', 'Mine'),
                    ('S', 'Spouse'),
-                   ('C', 'Child'),)
+                   ('D', 'Dependents'),)
 
 
 class SourceDoc(models.Model):
-    user = models.ForeignKey('member.TaxPayerProfile', blank=True, null=True)
-    category = models.CharField(max_length=10, choices=CATEGORY_CHOICE,
-                                null=True, blank=True)
+    tax_payer = models.ForeignKey('member.TaxPayerProfile', blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICE)
 
     def __str__(self):
         return self.category
