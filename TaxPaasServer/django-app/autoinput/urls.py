c08a4e98cd26a0bedce69261017e6172b0f58d02
Yunsoo Jung <yunsoo3042@gmail.com>
2017-10-16 13:33:32 +0900

    update autoinput api urls

--- TaxPaasServer/django-app/autoinput/urls.py
+++ TaxPaasServer/django-app/autoinput/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 
-from autoinput.views import W2TaskCreateView, W2CreateView, W2DetailView
+from autoinput.views import W2TaskCreateView, W2CreateView, W2DetailView,\
+    SourceDocDetailView
 
 urlpatterns = [
-    url(r'w2/task/$', W2TaskCreateView.as_view(), name='w2_task_create'),
-    url(r'w2/create/$', W2CreateView.as_view(), name='w2_create'),
-    url(r'w2/(?P<category>\w+)/$', W2DetailView.as_view(), name='w2_detail'),
-
+    url(r'w2/task/(?P<category>\w+)/(?P<doc_order>\d+)/(?P<order>\d+)/$',
+        W2TaskCreateView.as_view(), name='w2_task_create'),
+    url(r'w2/create/(?P<category>\w+)/(?P<doc_order>\d+)/(?P<order>\d+)/$',
+        W2CreateView.as_view(), name='w2_create'),
+    url(r'w2/(?P<category>\w+)/(?P<doc_order>\d+)/(?P<order>\d+)/$',
+        W2DetailView.as_view(), name='w2_detail'),
+    url(r'source_doc/(?P<category>\w+)/(?P<doc_order>\d+)/$',
+        SourceDocDetailView.as_view(), name='source_doc_detail'),
 ]
