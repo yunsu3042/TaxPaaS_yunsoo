f3650fac843f3cc670cc345e86707fd66655e8a3
Yunsoo Jung <yunsoo3042@gmail.com>
2017-12-06 10:39:12 +0900

    change space & new lines

--- TaxPaasServer/django-app/autoinput/functions/int/total_process.py
+++ TaxPaasServer/django-app/autoinput/functions/int/total_process.py
@@ -1,10 +1,10 @@
 from . import pre_process, ocr_process, make_int_dic, post_process
 
-__all__ = ('int_total_proces', )
+__all__ = ('int_total_process', )
 
 
-def int_total_proces(url=None, img=None):
-    img_list, st, end = pre_process(img=img)
+def int_total_process(url=None, img=None):
+    img_list, st, end = pre_process(url=url, img=img)
     page = ocr_process(img_list=img_list)
     reference_dic = make_int_dic()
     rdbs = post_process(page=page, reference_dic=reference_dic, img=img)
