459abd776e7366194fdf9e0e52c74363e5d9b6c1
Yunsoo Jung <yunsoo3042@gmail.com>
2017-12-06 10:46:30 +0900

    make int_autocomplete, div_autocomplete function and it is for make source documents be moved to redis-queue

--- TaxPaasServer/django-app/autoinput/task_func.py
+++ TaxPaasServer/django-app/autoinput/task_func.py
@@ -5,9 +5,11 @@ from django_rq import job
 from rq import get_current_job
 
 from autoinput.functions.w2 import w2_total_process
-from autoinput.functions.int import int_total_proces
-from autoinput.models import Task, W2
-from autoinput.serializers import W2Serializer
+from autoinput.functions.int import int_total_process
+from autoinput.functions.div import div_total_process
+from autoinput.models import Task, W2, Ten99INT, Ten99DIV
+from autoinput.serializers import W2Serializer, Ten99INTSerializer,\
+    Ten99DIVSerializer
 
 ast.literal_eval("{'x':1, 'y':2}")
 
@@ -27,7 +29,7 @@ def w2_autocomplete(np_arr, pk):
     if task.result:
         rdbs = task.result
         w2 = W2.objects.filter(pk=pk)[0]
-        save_model(instance=w2, data=rdbs, st=st, end=end)
+        save_model(instance=w2, data=rdbs, st=st, end=end, serializer=W2Serializer)
 
         # 완성 알림 보내기
         pass
@@ -38,14 +40,15 @@ def w2_autocomplete(np_arr, pk):
     return task.result
 
 
-def save_model(instance=None, data=None, st=None, end=None):
+def save_model(instance=None, data=None, st=None, end=None, serializer=None):
     data['cut_start_points'] = str(st)
     data['cut_end_points'] = str(end)
-    serializer = W2Serializer(instance, data=data, partial=True)
+    serializer = serializer(instance, data=data, partial=True)
     serializer.is_valid(raise_exception=True)
     serializer.save()
 
 
+@job
 def int_autocomplete(np_arr, pk):
     job = get_current_job()
 
@@ -53,10 +56,36 @@ def int_autocomplete(np_arr, pk):
         job_id=job.get_id()
     )
     img = Image.fromarray(np_arr)
-    task.result, st, end = int_total_proces(img=img)
+    task.result, st, end = int_total_process(img=img)
 
     if task.result:
         rdbs = task.result
+        int_obj = Ten99INT.objects.filter(pk=pk)[0]
+        save_model(instance=int_obj, data=rdbs, st=st, end=end,
+                   serializer=Ten99INTSerializer)
         pass
-    return None
+    else:
+        pass
+    task.save()
+    return task.result
 
+@job
+def div_autocomplete(np_arr, pk):
+    job = get_current_job()
+
+    task = Task.objects.create(
+        job_id=job.get_id()
+    )
+    img = Image.fromarray(np_arr)
+    task.result, st, end = div_total_process(img=img)
+
+    if task.result:
+        rdbs = task.result
+        div = Ten99DIV.objects.filter(pk=pk)[0]
+        save_model(instance=div, data=rdbs, st=st, end=end,
+                   serializer=Ten99DIVSerializer)
+        pass
+    else:
+        pass
+    task.save()
+    return task.result
